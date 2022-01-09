32ded58762e08a27fde585568797a22cadc3799f
choiman1559 <cuj1559@gmail.com>
2022-01-10 02:09:23 +0900

    history feature improvement

--- app/src/main/java/com/noti/main/ui/prefs/HistoryViewAdapter.java
+++ app/src/main/java/com/noti/main/ui/prefs/HistoryViewAdapter.java
@@ -1,12 +1,14 @@
 package com.noti.main.ui.prefs;
 
-import static android.content.Context.MODE_PRIVATE;
-
 import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +21,11 @@ import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.noti.main.BuildConfig;
 import com.noti.main.R;
+import com.noti.main.service.NotiListenerService;
+import com.noti.main.ui.ToastHelper;
+import com.noti.main.utils.CompressStringUtil;
 import com.noti.main.utils.ThreadProxy;
 
 import org.json.JSONArray;
@@ -38,7 +44,7 @@ public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.
         this.mode = mode;
         this.mContext = mContext;
         this.defaultDrawable = ContextCompat.getDrawable(mContext, R.drawable.ic_launcher_background);
-        prefs = mContext.getSharedPreferences("com.noti.main_preferences", MODE_PRIVATE);
+        prefs = mContext.getSharedPreferences("com.noti.main_preferences", Context.MODE_PRIVATE);
     }
 
     @NonNull
@@ -53,24 +59,21 @@ public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.
     public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
         String Package = "null";
         String date = "1900.01.01 00:00:00";
-        String title = "Can't find Title";
+        String title = "Error: Can't find Title";
         String content = "Can't find content";
         String device = "null";
+        String information = "Can't find package : null";
 
         try {
             JSONObject obj = list.getJSONObject(position);
             PackageManager pm = mContext.getPackageManager();
 
-            Package = obj.getString("package");
             date = obj.getString("date");
+            Package = obj.getString("package");
             device = mode == 1 ? obj.getString("device") : "";
-            title = obj.getString("title");
-            content = obj.getString("text");
 
             final String finalPackage = Package;
             final String finalDate = date;
-            final String finalTitle = title;
-            final String finalContent = content;
             final String finalDevice = device;
 
             ThreadProxy.getInstance().execute(() -> {
@@ -83,10 +86,18 @@ public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.
                 }
             });
 
+            information = String.format("%s (%s) %s", pm.getApplicationLabel(pm.getApplicationInfo(Package, PackageManager.GET_META_DATA)), Package, date);
+
+            title = obj.getString("title");
+            content = obj.getString("text");
+
+            final String finalTitle = title;
+            final String finalContent = content;
+
             holder.title.setSelected(true);
             holder.title.setText(title.equals("") ? "No Title" : title);
             holder.information.setSelected(true);
-            holder.information.setText(String.format("%s (%s) %s", pm.getApplicationLabel(pm.getApplicationInfo(Package, PackageManager.GET_META_DATA)), Package, date));
+            holder.information.setText(information);
 
             holder.setOnViewClickListener(() -> {
                 try {
@@ -102,6 +113,13 @@ public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.
                     builder.setTitle("Notification Details");
                     builder.setMessage(Html.fromHtml(message));
                     builder.setPositiveButton("Close", (dialog, which) -> { });
+                    builder.setNeutralButton("Send Again",  (dialog, which) -> {
+                        if(!prefs.getString("UID", "").equals("") && prefs.getBoolean("serviceToggle", false) && !prefs.getString("service", "reception").equals("reception")) {
+                            sendNormalNotificationAgain(finalPackage, finalDate, finalTitle, finalContent);
+                        } else {
+                            ToastHelper.show(mContext, "Please check service type and toggle and try again!","DISMISS", ToastHelper.LENGTH_SHORT);
+                        }
+                    });
                     builder.show();
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -115,6 +133,7 @@ public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.
             final String finalDevice = device;
 
             holder.title.setText(title);
+            holder.information.setText(information);
             holder.setOnViewClickListener(() -> {
                 String message = "";
                 message += "<b>App Package : </b>" + finalPackage + "<br>";
@@ -215,5 +234,76 @@ public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.
         public void setOnViewClickListener(OnViewClickListener listener) {
             if(listener != null) this.mListener = listener;
         }
+    }
+
+    void sendNormalNotificationAgain(String PackageName, String DATE, String TITLE, String TEXT) {
+        boolean isLogging = BuildConfig.DEBUG;
+        PackageManager pm = mContext.getPackageManager();
+        Bitmap ICON = null;
+        try {
+            ICON = NotiListenerService.getBitmapFromDrawable(pm.getApplicationIcon(PackageName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String ICONS;
+        if (ICON != null && prefs.getBoolean("SendIcon", false)) {
+            ICON.setHasAlpha(true);
+            int res;
+            switch (prefs.getString("IconRes", "")) {
+                case "68 x 68 (Not Recommend)":
+                    res = 68;
+                    break;
+
+                case "52 x 52 (Default)":
+                    res = 52;
+                    break;
+
+                case "36 x 36":
+                    res = 36;
+                    break;
+
+                default:
+                    res = 0;
+                    break;
+            }
+            ICONS = res == 0 ? "none" : CompressStringUtil.compressString(CompressStringUtil.getStringFromBitmap(NotiListenerService.getResizedBitmap(ICON, res, res)));
+        } else ICONS = "none";
+
+        String DEVICE_NAME = Build.MANUFACTURER + " " + Build.MODEL;
+        String DEVICE_ID = NotiListenerService.getMACAddress();
+        String TOPIC = "/topics/" + prefs.getString("UID", "");
+        String APPNAME = null;
+        try {
+            APPNAME = "" + pm.getApplicationLabel(pm.getApplicationInfo(PackageName, PackageManager.GET_META_DATA));
+        } catch (PackageManager.NameNotFoundException e) {
+            if (isLogging) Log.d("Error", "Package not found : " + PackageName);
+        }
+
+        if (isLogging) Log.d("length", String.valueOf(ICONS.length()));
+        JSONObject notificationHead = new JSONObject();
+        JSONObject notificationBody = new JSONObject();
+        try {
+            notificationBody.put("type", "send|normal");
+            notificationBody.put("title", TITLE != null ? TITLE : prefs.getString("DefaultTitle", "New notification"));
+            notificationBody.put("message", TEXT != null ? TEXT : prefs.getString("DefaultMessage", "notification arrived."));
+            notificationBody.put("package", PackageName);
+            notificationBody.put("appname", APPNAME);
+            notificationBody.put("device_name", DEVICE_NAME);
+            notificationBody.put("device_id", DEVICE_ID);
+            notificationBody.put("date", DATE);
+            notificationBody.put("icon", ICONS);
+
+            int dataLimit = prefs.getInt("DataLimit", 4096);
+            notificationHead.put("to", TOPIC);
+            notificationHead.put("android", new JSONObject().put("priority", "high"));
+            notificationHead.put("priority", 10);
+            notificationHead.put("data", notificationBody.toString().length() < dataLimit - 20 ? notificationBody : notificationBody.put("icon", "none"));
+        } catch (JSONException e) {
+            if (isLogging) Log.e("Noti", "onCreate: " + e.getMessage());
+        }
+        if (isLogging) Log.d("data", notificationHead.toString());
+        NotiListenerService.sendNotification(notificationHead, PackageName, mContext);
+        ToastHelper.show(mContext, "Your request is posted!","OK", ToastHelper.LENGTH_SHORT);
     }
 }
